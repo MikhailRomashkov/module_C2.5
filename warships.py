005452056f91ced80d3d4cad7a00a48eea42fdcd
Mikhail <m-romashkov@mail.ru>
2023-02-06 15:58:11 +1000

    create board

---
+++ warships.py
@@ -0,0 +1,29 @@
+#Create GameBoard
+class Board:
+    def __init__(self, hid = False, size = 6):
+        self.size = size
+        self.hid = hid
+
+        self.count = 0
+
+        self.field = [["O"] * size for _ in range(size)]
+
+        self.busy = []
+        self.ships = []
+
+    def __str__(self):
+        res = ""
+        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f"\n{i + 1} | " + " | ".join(row) + " |"
+
+        if self.hid:
+            res = res.replace("■", "O")
+        return res
+
+    def out(self, d):
+        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
+
+
+b = Board()
+print(b)
