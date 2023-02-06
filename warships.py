c327413216a0264a1e26dd7fd54488a439c586c3
Mikhail <m-romashkov@mail.ru>
2023-02-07 01:51:31 +1000

    add class Player

--- warships.py
+++ warships.py
@@ -125,3 +125,54 @@ class Board:
     def begin(self):
         self.busy = []
 
+
+# Класс Игрока
+class Player:
+    def __init__(self, board, enemy):
+        self.board = board
+        self.enemy = enemy
+
+
+    def ask(self):
+        raise NotImplementedError()
+
+    def move(self):
+        while True:
+            try:
+                target = self.ask()
+                repeat = self.enemy.shot(target)
+                return repeat
+            except BoardError as e:
+                print(e)
+
+
+class AI(Player):
+    def ask(self):
+        d =Dot(randint(0, 5), randint(0, 5))
+        time.sleep(5)
+        print(f'Ход Компьютера: {d.x + 1}, {d.y + 1}')
+        return d
+
+
+class User(Player):
+    def ask (self):
+        while True:
+            cords = input('Ваш ход: ')
+
+            if len(cords) != 2
+                print('Введите две координаты выстрела: ')
+                continue
+
+            x, y = cords
+
+            if not(x.isdigit()) or not(y.isdigit()):
+                print(" Введите числа! ")
+                continue
+
+            x, y = int(x), int(y)
+
+            return Dot(x - 1, y - 1)
+
+
+
+
